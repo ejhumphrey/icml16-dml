2318758b956e68837de6f0d5bf9b0a728883e3ab
Eric J. Humphrey <ejhumphrey@nyu.edu>
2016-04-30 10:41:07 -0400

    More data sampling tests.

--- dml/tests/test_data.py
+++ dml/tests/test_data.py
@@ -37,3 +37,28 @@ def test_pitch_neighbors(dataset):
 def test_instrument_pitch_neighbors(dataset):
     nbs = D.instrument_pitch_neighbors(dataset)
     assert len(nbs) == 10
+
+
+def test_slice_cqt(dataset):
+    row = dataset.iloc[0]
+    n_in = 4
+    slicer = D.slice_cqt(row, window_length=n_in)
+    x, y = next(slicer)
+
+    # TODO: Return to this (is the shape right?)
+    assert x.shape == (1, 1, n_in, 192)
+    assert y['instrument'] == row.instrument
+    assert y['note_number'] == row.note_number
+    assert y['fcode'] == row.fcode
+
+
+def test_neighbor_stream(dataset):
+    n_in = 4
+    nbs = D.instrument_neighbors(dataset)
+    stream = D.neighbor_stream(
+        nbs, dataset, D.slice_cqt, working_size=2, lam=5,
+        window_length=n_in)
+    data = next(stream)
+    assert data['x_in'].shape == (1, 1, n_in, 192)
+    assert data['x_same'].shape == (1, 1, n_in, 192)
+    assert data['x_diff'].shape == (1, 1, n_in, 192)
